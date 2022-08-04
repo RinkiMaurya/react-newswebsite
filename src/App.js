1028ddc46be547e12eb458756f9f622cde4d6d68
RinkiMaurya <rinkimaurya444@gmail.com>
2022-08-04 14:34:36 +0530

    Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {Card , Button} from "antd";
+import axios from "axios";
+import React,{ useEffect, useState } from 'react';
 
+const {Meta} = Card ;
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect ( ()=> {
+    const loadNews = async()=>{
+      const response = await axios.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=dcf25d889b7b49fc9066776bc15a6dee")
+      setNews(response.data.articles);
+    };
+
+    loadNews();
+  },[]);
+
+  console.log("news", news);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              {news && news.map((item , index)=> {
+                return(
+                  <Card 
+                  key={index}
+                  hoverable
+                  style={{width : "100%"}}
+                  cover = {<img alt = "images" src= {item.urlToImage}/>} >
+                  <Meta title= {item.title} description = {item.content}/>
+                  <a href={item.url} target="_blank" rel="noopnner">
+                  <Button type='primary' style={{marginTop: "10px"}}>Read More</Button>
+                  </a>
+                  </Card>
+
+                )
+              }
+              )}
     </div>
   );
 }
