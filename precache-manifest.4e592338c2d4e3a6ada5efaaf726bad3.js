dd0205d8b8a7458aef215600672f45b76d2d356c
RinkiMaurya <rinkimaurya444@gmail.com>
2022-08-04 15:21:39 +0530

    Updates

---
+++ precache-manifest.4e592338c2d4e3a6ada5efaaf726bad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0224ef26df6fafdeb46",
+    "url": "/react-newswebsite/static/css/main.e8394a13.chunk.css"
+  },
+  {
+    "revision": "b0224ef26df6fafdeb46",
+    "url": "/react-newswebsite/static/js/main.b0224ef2.chunk.js"
+  },
+  {
+    "revision": "0b4b0cffc21b1b3de579",
+    "url": "/react-newswebsite/static/js/1.0b4b0cff.chunk.js"
+  },
+  {
+    "revision": "79c88bd7f5032a201d83",
+    "url": "/react-newswebsite/static/css/2.33ba2f34.chunk.css"
+  },
+  {
+    "revision": "79c88bd7f5032a201d83",
+    "url": "/react-newswebsite/static/js/2.79c88bd7.chunk.js"
+  },
+  {
+    "revision": "b21b6c9a7f6fa9fe6206",
+    "url": "/react-newswebsite/static/js/runtime~main.b21b6c9a.js"
+  },
+  {
+    "revision": "2366ba793500fe57247bb3dfaf1e1692",
+    "url": "/react-newswebsite/index.html"
+  }
+];
